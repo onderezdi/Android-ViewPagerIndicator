2e7192ebd1410c684d59b3196f439b7049e497b7
ondere <onderezdi@gmail.com>
2020-09-28 23:38:09 +0300

    - url change

--- build/generated/source/buildConfig/release/com/viewpagerindicator/BuildConfig.java
+++ build/generated/source/buildConfig/release/com/viewpagerindicator/BuildConfig.java
@@ -5,9 +5,14 @@ package com.viewpagerindicator;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "com.viewpagerindicator";
+  public static final String LIBRARY_PACKAGE_NAME = "com.viewpagerindicator";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.viewpagerindicator";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 65;
-  public static final String VERSION_NAME = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
 }
